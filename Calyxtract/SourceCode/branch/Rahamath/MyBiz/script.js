807c602564cf9f3045534a18b361047d89a687d2
rahamath <rahamath1298@gmail.com>
2021-05-28 01:05:43 +0530

    newly added
    updated files

---
+++ Calyxtract/SourceCode/branch/Rahamath/MyBiz/script.js
@@ -0,0 +1,255 @@
+/*r0*/
+document.querySelector('.show-more-button').addEventListener('click', function() {
+  // If text is shown less, then show complete
+  if(this.getAttribute('data-more') == 0) {
+    this.setAttribute('data-more', 1);
+    this.style.display = 'block';
+    this.innerHTML = 'Read Less';
+
+    this.previousElementSibling.style.display = 'none';
+    this.previousElementSibling.previousElementSibling.style.display = 'inline';
+  }
+  // If text is shown complete, then show less
+  else if(this.getAttribute('data-more') == 1) {
+    this.setAttribute('data-more', 0);
+    this.style.display = 'inline';
+    this.innerHTML = 'Read More';
+
+    this.previousElementSibling.style.display = 'inline';
+    this.previousElementSibling.previousElementSibling.style.display = 'none';
+  } 
+});
+
+/*r1*/
+document.querySelector('.show-more-button1').addEventListener('click', function() {
+  // If text is shown less, then show complete
+  if(this.getAttribute('data-more') == 0) {
+    this.setAttribute('data-more', 1);
+    this.style.display = 'block';
+    this.innerHTML = 'Read Less';
+
+    this.previousElementSibling.style.display = 'none';
+    this.previousElementSibling.previousElementSibling.style.display = 'inline';
+  }
+  // If text is shown complete, then show less
+  else if(this.getAttribute('data-more') == 1) {
+    this.setAttribute('data-more', 0);
+    this.style.display = 'inline';
+    this.innerHTML = 'Read More';
+
+    this.previousElementSibling.style.display = 'inline';
+    this.previousElementSibling.previousElementSibling.style.display = 'none';
+  } 
+});
+
+/*r2*/
+
+document.querySelector('.show-more-button2').addEventListener('click', function() {
+  // If text is shown less, then show complete
+  if(this.getAttribute('data-more') == 0) {
+    this.setAttribute('data-more', 1);
+    this.style.display = 'block';
+    this.innerHTML = 'Read Less';
+
+    this.previousElementSibling.style.display = 'none';
+    this.previousElementSibling.previousElementSibling.style.display = 'inline';
+  }
+  // If text is shown complete, then show less
+  else if(this.getAttribute('data-more') == 1) {
+    this.setAttribute('data-more', 0);
+    this.style.display = 'inline';
+    this.innerHTML = 'Read More';
+
+    this.previousElementSibling.style.display = 'inline';
+    this.previousElementSibling.previousElementSibling.style.display = 'none';
+  } 
+});
+
+/*r3*/
+
+document.querySelector('.show-more-button3').addEventListener('click', function() {
+  // If text is shown less, then show complete
+  if(this.getAttribute('data-more') == 0) {
+    this.setAttribute('data-more', 1);
+    this.style.display = 'block';
+    this.innerHTML = 'Read Less';
+
+    this.previousElementSibling.style.display = 'none';
+    this.previousElementSibling.previousElementSibling.style.display = 'inline';
+  }
+  // If text is shown complete, then show less
+  else if(this.getAttribute('data-more') == 1) {
+    this.setAttribute('data-more', 0);
+    this.style.display = 'inline';
+    this.innerHTML = 'Read More';
+
+    this.previousElementSibling.style.display = 'inline';
+    this.previousElementSibling.previousElementSibling.style.display = 'none';
+  } 
+});
+
+/*r4*/
+
+document.querySelector('.show-more-button4').addEventListener('click', function() {
+  // If text is shown less, then show complete
+  if(this.getAttribute('data-more') == 0) {
+    this.setAttribute('data-more', 1);
+    this.style.display = 'block';
+    this.innerHTML = 'Read Less';
+
+    this.previousElementSibling.style.display = 'none';
+    this.previousElementSibling.previousElementSibling.style.display = 'inline';
+  }
+  // If text is shown complete, then show less
+  else if(this.getAttribute('data-more') == 1) {
+    this.setAttribute('data-more', 0);
+    this.style.display = 'inline';
+    this.innerHTML = 'Read More';
+
+    this.previousElementSibling.style.display = 'inline';
+    this.previousElementSibling.previousElementSibling.style.display = 'none';
+  } 
+});
+
+/*r5*/
+
+document.querySelector('.show-more-button5').addEventListener('click', function() {
+  // If text is shown less, then show complete
+  if(this.getAttribute('data-more') == 0) {
+    this.setAttribute('data-more', 1);
+    this.style.display = 'block';
+    this.innerHTML = 'Read Less';
+
+    this.previousElementSibling.style.display = 'none';
+    this.previousElementSibling.previousElementSibling.style.display = 'inline';
+  }
+  // If text is shown complete, then show less
+  else if(this.getAttribute('data-more') == 1) {
+    this.setAttribute('data-more', 0);
+    this.style.display = 'inline';
+    this.innerHTML = 'Read More';
+
+    this.previousElementSibling.style.display = 'inline';
+    this.previousElementSibling.previousElementSibling.style.display = 'none';
+  } 
+});
+
+/*r6*/
+
+document.querySelector('.show-more-button6').addEventListener('click', function() {
+  // If text is shown less, then show complete
+  if(this.getAttribute('data-more') == 0) {
+    this.setAttribute('data-more', 1);
+    this.style.display = 'block';
+    this.innerHTML = 'Read Less';
+
+    this.previousElementSibling.style.display = 'none';
+    this.previousElementSibling.previousElementSibling.style.display = 'inline';
+  }
+  // If text is shown complete, then show less
+  else if(this.getAttribute('data-more') == 1) {
+    this.setAttribute('data-more', 0);
+    this.style.display = 'inline';
+    this.innerHTML = 'Read More';
+
+    this.previousElementSibling.style.display = 'inline';
+    this.previousElementSibling.previousElementSibling.style.display = 'none';
+  } 
+});
+
+
+/*r7*/
+
+document.querySelector('.show-more-button7').addEventListener('click', function() {
+  // If text is shown less, then show complete
+  if(this.getAttribute('data-more') == 0) {
+    this.setAttribute('data-more', 1);
+    this.style.display = 'block';
+    this.innerHTML = 'Read Less';
+
+    this.previousElementSibling.style.display = 'none';
+    this.previousElementSibling.previousElementSibling.style.display = 'inline';
+  }
+  // If text is shown complete, then show less
+  else if(this.getAttribute('data-more') == 1) {
+    this.setAttribute('data-more', 0);
+    this.style.display = 'inline';
+    this.innerHTML = 'Read More';
+
+    this.previousElementSibling.style.display = 'inline';
+    this.previousElementSibling.previousElementSibling.style.display = 'none';
+  } 
+});
+
+/*r8*/
+
+document.querySelector('.show-more-button8').addEventListener('click', function() {
+  // If text is shown less, then show complete
+  if(this.getAttribute('data-more') == 0) {
+    this.setAttribute('data-more', 1);
+    this.style.display = 'block';
+    this.innerHTML = 'Read Less';
+
+    this.previousElementSibling.style.display = 'none';
+    this.previousElementSibling.previousElementSibling.style.display = 'inline';
+  }
+  // If text is shown complete, then show less
+  else if(this.getAttribute('data-more') == 1) {
+    this.setAttribute('data-more', 0);
+    this.style.display = 'inline';
+    this.innerHTML = 'Read More';
+
+    this.previousElementSibling.style.display = 'inline';
+    this.previousElementSibling.previousElementSibling.style.display = 'none';
+  } 
+});
+
+/*r9*/
+
+document.querySelector('.show-more-button9').addEventListener('click', function() {
+  // If text is shown less, then show complete
+  if(this.getAttribute('data-more') == 0) {
+    this.setAttribute('data-more', 1);
+    this.style.display = 'block';
+    this.innerHTML = 'Read Less';
+
+    this.previousElementSibling.style.display = 'none';
+    this.previousElementSibling.previousElementSibling.style.display = 'inline';
+  }
+  // If text is shown complete, then show less
+  else if(this.getAttribute('data-more') == 1) {
+    this.setAttribute('data-more', 0);
+    this.style.display = 'inline';
+    this.innerHTML = 'Read More';
+
+    this.previousElementSibling.style.display = 'inline';
+    this.previousElementSibling.previousElementSibling.style.display = 'none';
+  } 
+});
+
+/*r10*/
+
+document.querySelector('.show-more-button10').addEventListener('click', function() {
+  // If text is shown less, then show complete
+  if(this.getAttribute('data-more') == 0) {
+    this.setAttribute('data-more', 1);
+    this.style.display = 'block';
+    this.innerHTML = 'Read Less';
+
+    this.previousElementSibling.style.display = 'none';
+    this.previousElementSibling.previousElementSibling.style.display = 'inline';
+  }
+  // If text is shown complete, then show less
+  else if(this.getAttribute('data-more') == 1) {
+    this.setAttribute('data-more', 0);
+    this.style.display = 'inline';
+    this.innerHTML = 'Read More';
+
+    this.previousElementSibling.style.display = 'inline';
+    this.previousElementSibling.previousElementSibling.style.display = 'none';
+  } 
+});
+
+
+
+
