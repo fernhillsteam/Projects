78a6abd8b4ee20e84dbbd5219b9b02225eb783ca
SunilPawar88 <pawar.sunil88@gmail.com>
2021-06-11 11:54:19 +0530

    CAN
    Single Master multi slave, data transmit to master without any request from master

---
+++ Calyxtract/SourceCode/branch/Sunil/CAN/CAN_slave_str1/Debug/subdir_vars.mk
@@ -0,0 +1,35 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../hello_ccs.cmd 
+
+C_SRCS += \
+../can_slave.c \
+../startup_ccs.c 
+
+C_DEPS += \
+./can_slave.d \
+./startup_ccs.d 
+
+OBJS += \
+./can_slave.obj \
+./startup_ccs.obj 
+
+OBJS__QUOTED += \
+"can_slave.obj" \
+"startup_ccs.obj" 
+
+C_DEPS__QUOTED += \
+"can_slave.d" \
+"startup_ccs.d" 
+
+C_SRCS__QUOTED += \
+"../can_slave.c" \
+"../startup_ccs.c" 
+
+
