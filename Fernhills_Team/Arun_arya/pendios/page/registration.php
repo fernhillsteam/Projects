8517d83ce203935eafa9d6352203c7f7fd0c0be3
ArunArya <19arunarya@gmail.com>
2021-06-29 13:56:16 +0530

    Forgot password
    Forgot password implemented

--- Fernhills_Team/Arun_arya/pendios/page/registration.php
+++ Fernhills_Team/Arun_arya/pendios/page/registration.php
@@ -64,5 +64,8 @@
 <?php
     }
 ?>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+ <script src="bootstrap-4.0.0/dist/js/bootstrap.js"></script>
+ <script src="../assets/js/core/bootstrap.min.js"></script>
 </body>
 </html>
---
+++ Fernhills_Team/Arun_arya/pendios/page/update0.php
@@ -0,0 +1,29 @@
+<?php
+include('auth_session.php');
+   require('db.php');
+
+ 
+  $colname = $_POST['colname'];
+  $query ="SELECT * FROM `users` WHERE `mobilenumber`='".$_SESSION['mobilenumber']."'";
+   $result = mysqli_query($con, $query) or die(mysql_error());
+  
+			 while($row = mysqli_fetch_row($result)){
+  if($colname == "btn1"){
+  $query = mysqli_query($result,"update `actions` set `send_sms` = 0 where `user_id` = '".$row[0]."'") or die("Could not find table".mysqli_error($result));
+}
+if($colname == "btn2"){
+	 $query = mysqli_query($result,"update `actions` set `update_demand` = 0 where `user_id` = '".$row[0]."'") or die("Could not find table".mysqli_error($result));
+}
+
+if($colname == "btn3"){
+$query = mysqli_query($result,"update `actions` set `authorize` = 0 where `user_id` = '".$row[0]."'") or die("Could not find table".mysqli_error($result));
+
+}
+if($colname == "btn4"){
+$query = mysqli_query($result,"update `actions` set `shutdown` = 0 where `user_id` = '".$row[0]."'") or die("Could not find table".mysqli_error($result));
+
+}
+
+
+			 }
+?>
