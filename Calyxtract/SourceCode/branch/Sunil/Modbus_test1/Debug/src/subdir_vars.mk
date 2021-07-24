7e1387252a6a149cc97abcc0d9424e455a52ef1b
SunilPawar88 <pawar.sunil88@gmail.com>
2021-07-24 11:06:50 +0530

    Modbus project with pneumatic/ Sunil Pawar
    Modbus project with pneumatic/ Sunil Pawar

---
+++ Calyxtract/SourceCode/branch/Sunil/Modbus_test1/Debug/src/subdir_vars.mk
@@ -0,0 +1,44 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/RS_232.c \
+../src/Timer0_A.c \
+../src/bicolor.c \
+../src/modbus.c 
+
+C_DEPS += \
+./src/RS_232.d \
+./src/Timer0_A.d \
+./src/bicolor.d \
+./src/modbus.d 
+
+OBJS += \
+./src/RS_232.obj \
+./src/Timer0_A.obj \
+./src/bicolor.obj \
+./src/modbus.obj 
+
+OBJS__QUOTED += \
+"src\RS_232.obj" \
+"src\Timer0_A.obj" \
+"src\bicolor.obj" \
+"src\modbus.obj" 
+
+C_DEPS__QUOTED += \
+"src\RS_232.d" \
+"src\Timer0_A.d" \
+"src\bicolor.d" \
+"src\modbus.d" 
+
+C_SRCS__QUOTED += \
+"../src/RS_232.c" \
+"../src/Timer0_A.c" \
+"../src/bicolor.c" \
+"../src/modbus.c" 
+
+
