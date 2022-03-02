270349a15e03c162a726a01000465f1b9ead9d9c
rahamath <rahamath1298@gmail.com>
2022-03-02 14:40:49 +0530

    update
    update

--- Fernhills_Team/Arun_arya/vto_v0.3/config.php
+++ Fernhills_Team/Arun_arya/vto_v0.3/config.php
@@ -1,3 +1,4 @@
+
 <?php
 
 
@@ -6,19 +7,9 @@ define('DB_NAME', 'u152216168_vto');
 define('DB_USER', 'u152216168_vto');
 define('DB_PASS', 'Vto@1234567890');
 
-
-?>
-
-<?php
-
-
-/*define('DB_HOST', 'localhost');
-define('DB_NAME', 'u152216168_vto');
-define('DB_USER', 'u152216168_vto');
-define('DB_PASS', 'Vto@1234');*/
-
 /*define('DB_HOST', 'localhost');
 define('DB_NAME', 'u152216168_vto');
 define('DB_USER', 'root');
 define('DB_PASS', 'root@123');*/
+
 ?>
